2c40b7f0319437ccf7aaece98d5954139945b5ab
Likem <likemdzokoto@gmail.com>
2022-07-08 04:14:24 +0000

    1-dlistint_len

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -4,9 +4,6 @@
  * dlistint_len - returns the number of elements in
  * a double linked list
  *
- * @h: head of the list
- * Return: the number of nodes
- * Author: Martins Akhivbareme
  */
 size_t dlistint_len(const dlistint_t *h)
 {
