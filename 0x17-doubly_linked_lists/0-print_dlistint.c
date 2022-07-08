86fcce46f48ac52e2b24fa27d255c2d380bd7bbe
Likem <likemdzokoto@gmail.com>
2022-07-08 04:08:44 +0000

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -4,9 +4,6 @@
  * print_dlistint - prints all the elements of a
  * dlistint_t list
  *
- * @h: head of the list
- * Return: the number of nodes
- * Author: Martins Akhivbareme
  */
 size_t print_dlistint(const dlistint_t *h)
 {
